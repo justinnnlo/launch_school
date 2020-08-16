6c6aecca99de47cef9434fa57407755767d59a4f
Jose de la Puente <14jdelap@gmail.com>
2020-08-16 17:37:13 +0200

    Notes and more for basics course

--- Ruby-Basics/introduction_to_programming_with_ruby/06_Loops_and_Iterators/exercises.rb
+++ Ruby-Basics/introduction_to_programming_with_ruby/06_Loops_and_Iterators/exercises.rb
@@ -1,3 +1,38 @@
+require "byebug"
+# 1. What does the each method in the following program return after it is finished executing?
+
+x = [1, 2, 3, 4, 5] #each returns the original object, in this case: [1, 2, 3, 4, 5]
+x.each do |a|
+  a + 1
+end
+
+# 2. Write a while loop that takes input from the user, performs an action, and only stops when the user types "STOP". Each loop can get info from the user.
+
+p "Write START to commence"
+input = gets.chomp
+while input != "END"
+  p "We're in a MERRY-GO-ROUND"
+  p "END or not?"
+  input = gets.chomp
+end
+
+# 3. Write a method that counts down to zero using recursion.
+
+def to_zero(x)
+  if x > 0
+    p x -= 1
+    to_zero(x)
+  elsif x == 0
+    return "x is 0!"
+  else
+    return "Something's wrong — x can't be smaller than 0!"
+  end
+end
+
+
+
+# My ORIGINAL trial
+
 # Problem 1: What does the each method in the following program return after it is finished executing?
 
 x = [1, 2, 3, 4, 5]
